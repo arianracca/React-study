52eb29aa2c07931c5ca6936c42ba47ed5d6d05ca
Arian Racca <arianracca@gmail.com>
2022-11-28 13:55:00 -0300

    iniciando

--- 03.counter-app-vite/src/main.jsx
+++ 03.counter-app-vite/src/main.jsx
@@ -7,8 +7,11 @@ function App() {
     return (<h1>Hola Mundo</h1>)
 }
 
+//Renderizado de la aplicación
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App/>
     </React.StrictMode>
 )
+
+[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=arianracca&count_private=true&show_icons=true&theme=dracula)](https://github.com/anuraghazra/github-readme-stats)
