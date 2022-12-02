bf9386fcb24efa31b085306142effd8a4d6e6eff
Arian Racca <arianracca@gmail.com>
2022-12-02 15:54:58 -0300

    01

--- 04.gif-expert-app/src/GifExpertApp.jsx
+++ 04.gif-expert-app/src/GifExpertApp.jsx
@@ -1,23 +1,33 @@
 import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
   //Esto es un Hook (nunca poner condicionales a los hooks)
   const [categories, setCategories] = useState(['One Punch','Dragon Ball']);
 
+  const onAddCategory = (newCategory) => {
+    console.log(newCategory)
+
+    //Dos modos posibles de añadir nuevas categorias
+    //setCategories([...categories,'Valorant'])
+    //setCategories(cat => [...cat, '12']);
+
+  }
+
   return (
     <>
         {/*Titulo*/}
         <h1>GifExpertApp</h1>
-        {/*Input*/}
 
-        {/*Listado de Gif*/}
+        <AddCategory
+          //setCategories={setCategories}
+          onNewCategory={(value) => onAddCategory(value)}
+        />
         <ol>
           {categories.map(category => {
             return <li key={category}>{category}</li>
           })}
         </ol>
-
-
             {/*Gif Item*/}
     </>
   )
